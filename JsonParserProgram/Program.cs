b5f7d0867dc835363fa6780b0a8cafd04e0d1fd2
Alexander Bruun <39629240+alxbrn@users.noreply.github.com>
2020-02-22 13:39:40 +0100

    Add files via upload

---
+++ JsonParserProgram/Program.cs
@@ -0,0 +1,60 @@
+﻿using JsonParser;
+using System.Collections.Generic;
+using System.IO;
+using System;
+
+namespace JsonParserProgram
+{
+    class School
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public School(int id, string name)
+        {
+            Id = id;
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Name: {Name}";
+        }
+    }
+
+    class Person
+    {
+        public int Id { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public School School { get; set; }
+
+        public Person(int id, string firstname, string lastname, School school)
+        {
+            Id = id;
+            Firstname = firstname;
+            Lastname = lastname;
+            School = school;
+        }
+
+        public override string ToString()
+        {
+            return $"{{Id: {Id}, Firstname: {Firstname}, Lastname: {Lastname}, School: {{{School}}}}}";
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Person person = new Person(1, "Alexander", "Bruun", new School(72, "Zealand"));
+            string json = person.Parse();
+
+            Console.WriteLine(json);
+
+            Person newPerson = json.Parse<Person>();
+
+            Console.WriteLine(newPerson);
+        }
+    }
+}
